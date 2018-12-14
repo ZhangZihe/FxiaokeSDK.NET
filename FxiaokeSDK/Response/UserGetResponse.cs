42323d3dc1550a55649f19d90e753e8554e0d5a4
wangwenhao <wangwenhao@sodao.com>
2018-12-14 17:07:53 +0800

    员工信息添加Email字段

--- FxiaokeSDK/Response/UserGetResponse.cs
+++ FxiaokeSDK/Response/UserGetResponse.cs
@@ -10,6 +10,7 @@ namespace FxiaokeSDK.Response
         public string Name { get; set; }
         public string NickName { get; set; }
         public bool IsStop { get; set; }
+        public string Email { get; set; }
         public string Mobile { get; set; }
         public string Gender { get; set; }
         public string Position { get; set; }
